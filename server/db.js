30f321c81f39caf05029340ae3bfe9a78ceec733
vinitvinay <vinaymasai1998@gmail.com>
2023-06-13 17:44:35 +0530

    final

--- server/db.js
+++ server/db.js
@@ -1,11 +1,9 @@
-const mongoose=require("mongoose")
+const mongoose = require("mongoose");
 
-require("dotenv").config()
+require("dotenv").config();
 
-const connection=mongoose.connect(process.env.MongoUrl)
+const connection = mongoose.connect(process.env.MongoUrl);
 
-module.exports={
-    connection
-}
-
-
+module.exports = {
+  connection,
+};
